c52664aabb1cf54d10c749226cd53776524b9cd1
andrei_b <andrei.borovsky@gmail.com>
2026-01-07 13:42:27 +0100

    Remove build artifacts from repository

--- _codeql_build_dir/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/qtpyt/qtpyt/tests
-# Build directory: /home/runner/work/qtpyt/qtpyt/_codeql_build_dir/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/runner/work/qtpyt/qtpyt/_codeql_build_dir/tests/qtpyt_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
