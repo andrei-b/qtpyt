d3412526c8f068f96f5c8736b0d630f40aee5065
andrei-b <andrei-b@users.noreply.github.com>
2026-01-08 22:39:37 +0000

    deploy: 715cdea9ed49ba25dd0730f0c7299c9d29b1954b

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['d_5f_0',['d_',['../classqtpyt_1_1QPySharedArray.html#ad4d762b7e043c106d43c207c145cea70',1,'qtpyt::QPySharedArray']]],
-  ['data_1',['data',['../structqtpyt_1_1QPySharedArray_1_1Data.html',1,'qtpyt::QPySharedArray&lt; T &gt;::Data'],['../classqtpyt_1_1QPySharedArray.html#a8513f9cc35daa34afe22809502d5c04f',1,'qtpyt::QPySharedArray::data()'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a36a0b8b6179ee9a75dec0cafeda6fc1f',1,'qtpyt::QPySharedArray::Data::Data()=default'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a90bb1fc7771572e19f8f0bf7d642afe4',1,'qtpyt::QPySharedArray::Data::Data(const Data &amp;o)']]],
+  ['data_1',['data',['../structqtpyt_1_1QPySharedArray_1_1Data.html',1,'qtpyt::QPySharedArray&lt; T &gt;::Data'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a36a0b8b6179ee9a75dec0cafeda6fc1f',1,'qtpyt::QPySharedArray::Data::Data()=default'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a90bb1fc7771572e19f8f0bf7d642afe4',1,'qtpyt::QPySharedArray::Data::Data(const Data &amp;o)'],['../classqtpyt_1_1QPySharedArray.html#a8513f9cc35daa34afe22809502d5c04f',1,'qtpyt::QPySharedArray::data()']]],
   ['data_20types_20for_20zero_20copy_20transfer_2',['Special Data Types for Zero-Copy Transfer',['../type_conversion.html#special_types',1,'']]],
   ['deleter_3',['deleter',['../structqtpyt_1_1detail_1_1OwnerState.html#ac37d12668b53344b2a0cfd44cfb4998b',1,'qtpyt::detail::OwnerState']]],
   ['detach_4',['detach',['../classqtpyt_1_1QPySharedArray.html#acfa8738857cfdc070d3efbb13f28e915',1,'qtpyt::QPySharedArray']]],
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['data_0',['data',['../classqtpyt_1_1QPySharedArray.html#a8513f9cc35daa34afe22809502d5c04f',1,'qtpyt::QPySharedArray::data()'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a36a0b8b6179ee9a75dec0cafeda6fc1f',1,'qtpyt::QPySharedArray::Data::Data()=default'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a90bb1fc7771572e19f8f0bf7d642afe4',1,'qtpyt::QPySharedArray::Data::Data(const Data &amp;o)']]],
+  ['data_0',['data',['../structqtpyt_1_1QPySharedArray_1_1Data.html#a36a0b8b6179ee9a75dec0cafeda6fc1f',1,'qtpyt::QPySharedArray::Data::Data()=default'],['../structqtpyt_1_1QPySharedArray_1_1Data.html#a90bb1fc7771572e19f8f0bf7d642afe4',1,'qtpyt::QPySharedArray::Data::Data(const Data &amp;o)'],['../classqtpyt_1_1QPySharedArray.html#a8513f9cc35daa34afe22809502d5c04f',1,'qtpyt::QPySharedArray::data()']]],
   ['detach_1',['detach',['../classqtpyt_1_1QPySharedArray.html#acfa8738857cfdc070d3efbb13f28e915',1,'qtpyt::QPySharedArray']]]
 ];
