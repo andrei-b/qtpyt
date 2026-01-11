3775a25c60a3ff26d1d4420718abbf96a93ed61e
andrei-b <andrei-b@users.noreply.github.com>
2026-01-11 12:16:06 +0000

    deploy: 25cea67ac773d0075c88b78f866d785d5882f01c

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['getinternal_0',['getInternal',['../classqtpyt_1_1QPyModuleBase.html#a24d96e2c8bad973683dd53c50bc96b62',1,'qtpyt::QPyModuleBase']]],
-  ['getmethodindex_1',['getMethodIndex',['../classqtpyt_1_1QPySlot.html#ae7a0c2f483e94639c150d7571a8749b8',1,'qtpyt::QPySlot']]],
+  ['getmethodindex_1',['getmethodindex',['../classqtpyt_1_1QPyModule.html#a0229d2618cc5cc487bd5db4233244c99',1,'qtpyt::QPyModule::getMethodIndex()'],['../classqtpyt_1_1QPySlot.html#ae7a0c2f483e94639c150d7571a8749b8',1,'qtpyt::QPySlot::getMethodIndex()']]],
   ['getpointer_2',['getPointer',['../classqtpyt_1_1QPySequenceReference.html#a9e1c88658900716731a80b5eb0827a04',1,'qtpyt::QPySequenceReference']]],
   ['getsignalindex_3',['getSignalIndex',['../classqtpyt_1_1QPySlot.html#a3484e3cfb39e073ebb3fe51f32bdbc8c',1,'qtpyt::QPySlot']]],
   ['getthreadid_4',['getThreadId',['../classqtpyt_1_1QPyModule.html#ad20e96958ad5e2f851d620ada570cd09',1,'qtpyt::QPyModule']]]
