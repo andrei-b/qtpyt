7530fdb1ec84d1b77565e508aeadca4395e052d9
andrei_b <andrei.borovsky@gmail.com>
2026-01-11 15:17:31 +0100

    removed meindex

--- include/qtpyt/qpymodule.h
+++ include/qtpyt/qpymodule.h
@@ -20,8 +20,7 @@
 
 #include <QObject>
 #include <QRunnable>
-#include <private/qmetaobject_p.h>
-
+#include "methodindex.h"
 
 #include "qpythreadpool.h"
 
@@ -203,41 +202,7 @@ protected:
     /// \brief Captures/sets the current thread id as the module's associated thread.
     void setThreadId();
 
-    template <typename SignalFunc>
-static std::optional<int> getMethodIndex(const QObject* sender, SignalFunc signal) {
-        if (!signal) {
-            qWarning("QObject::connect: invalid nullptr parameter");
-            return std::nullopt;
-        }
 
-        int localIndex = -1;
-        SignalFunc signalVar = signal;
-        void* args[] = { &localIndex, reinterpret_cast<void*>(&signalVar) };
-
-        const QMetaObject* originalMeta = sender->metaObject();
-        const QMetaObject* foundMeta = nullptr;
-
-        for (const QMetaObject* m = originalMeta; m && localIndex < 0; m = m->superClass()) {
-            m->static_metacall(QMetaObject::IndexOfMethod, 0, args);
-            if (localIndex >= 0 && localIndex < QMetaObjectPrivate::get(m)->signalCount) {
-                foundMeta = m;
-                break;
-            }
-        }
-
-        if (!foundMeta) {
-            qWarning("QObject::connect: signal not found in %s", originalMeta->className());
-            return std::nullopt;
-        }
-
-        int absoluteMethodIndex = foundMeta->methodOffset() + localIndex;
-        if (absoluteMethodIndex < 0 || absoluteMethodIndex >= originalMeta->methodCount()) {
-            qWarning("QObject::connect: computed method index out of range for %s", foundMeta->className());
-            return std::nullopt;
-        }
-
-        return absoluteMethodIndex;
-    }
 
 private:
     /// \brief Marks the module as wanting to cancel outstanding work.
